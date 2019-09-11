1bc73c983b5967c3a71c010535893b1fbdc00611
nearjay06 <nearjoan2018@gmail.com>
2019-09-11 18:10:00 +0300

    add number 9

--- practice.py
+++ practice.py
@@ -1,4 +1,4 @@
-# # No.1
+# No.1
 def info():
     a = input('Please enter your name: ')    #a represents the input
     b = int(input('Please enter your age: ')) # b represents the age
@@ -8,7 +8,7 @@ def info():
     print( a + " will become 100 years old in " + d)
 info()
 
-# # No.2
+ # No.2
 s = int(input('Enter a number: ')) 
 p = int((s / 2))
 if p % 2 == 0:
@@ -17,28 +17,28 @@ else:
     print('the number you chose is an odd number')
 
 
-# # No.3
+#No.3
 list = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 for items in list:
     if items < 5:
         print (items)
 
 
-# # No.4   
+#No.4   
 x = int(input('Enter a number: '))
 print ("The divisors are: ")
 for y in range(1,x+1):
     if(x % y == 0):
         print (y)
     
-#No.5
+# #No.5
 
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 c = [value for value in a if value in b] 
 print (c)
 
-#No.6
+# #No.6
 f= str(input('Enter a string here: '))
 d_str = 'dad'
 d_str = d_str.casefold()
@@ -48,13 +48,13 @@ if list(d_str) == list(rev_str):
 else:
     print('this word is not a palindrome') 
 
-#No.7
+# #No.7
 a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
 b = [items for items in a if items % 2 == 0]
 print(b)
 
-#No.8
-e =["rock", "paper", "scissors"]
+# #No.8
+# e =["rock", "paper", "scissors"]
 import sys
 player1 = input("What's your name?")
 player2 = input("And your name?")
@@ -87,11 +87,26 @@ print(compare(player1_answer, player2_answer))
 
 
 #No.9
-z = [1,2,3,4,5,6,7,8,9]
-guess = input('Guess any number here:')
-a += 1
+# z = [1,2,3,4,5,6,7,8,9]
+# guess = input('Guess any number here:')
+# a += 1
 
-#  count += 1
+import random
+
+number = random.randint(1,9)
+guess = 0
+count = 0
+
+
+while guess != number and guess != "exit":
+    guess = input("What's your guess?")
+    
+    if guess == "exit":
+        break
+    
+    guess = int(guess)
+    count += 1
+
 if guess < number:
     print("Too low!")
 elif guess > number:
