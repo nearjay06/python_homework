5faaf43c4fd129d8b23cfe7730ebbecb2d589ae2
nearjay06 <nearjoan2018@gmail.com>
2019-09-14 19:21:26 +0300

    did numbers 15 to 20 and put them in the practice.py file, modify trial.py file

--- practice.py
+++ practice.py
@@ -1,4 +1,4 @@
-# No.1
+# # No.1
 def info():
     a = input('Please enter your name: ')    #a represents the input
     b = int(input('Please enter your age: ')) # b represents the age
@@ -8,7 +8,7 @@ def info():
     print( a + " will become 100 years old in " + d)
 info()
 
- # No.2
+# No.2
 s = int(input('Enter a number: ')) 
 p = int((s / 2))
 if p % 2 == 0:
@@ -31,14 +31,14 @@ for y in range(1,x+1):
     if(x % y == 0):
         print (y)
     
-# #No.5
+#No.5
 
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 c = [value for value in a if value in b] 
 print (c)
 
-# #No.6
+#No.6
 f= str(input('Enter a string here: '))
 d_str = 'dad'
 d_str = d_str.casefold()
@@ -48,13 +48,13 @@ if list(d_str) == list(rev_str):
 else:
     print('this word is not a palindrome') 
 
-# #No.7
+#No.7
 a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
 b = [items for items in a if items % 2 == 0]
 print(b)
 
-# #No.8
-# e =["rock", "paper", "scissors"]
+#No.8
+e =["rock", "paper", "scissors"]
 import sys
 player1 = input("What's your name?")
 player2 = input("And your name?")
@@ -87,9 +87,9 @@ print(compare(player1_answer, player2_answer))
 
 
 #No.9
-# z = [1,2,3,4,5,6,7,8,9]
-# guess = input('Guess any number here:')
-# a += 1
+z = [1,2,3,4,5,6,7,8,9]
+guess = input('Guess any number here:')
+a += 1
 
 import random
 
@@ -132,9 +132,122 @@ for y in range(2, z):
     else:
         print('This is not a prime number')
 
-# No.12
+# # No.12
+
 a = [5,10,15,20,25,30,35,40]
 b = (a[0],a[-1])
 print(b)
 
-# No.13
+#No.13
+
+xterms = 11
+
+x1 = 0
+x2 = 1
+compute = 0
+
+if xterms <= 0:
+    print ('Enter a positive number')
+elif xterms == 1:
+   print("Fibonacci sequence upto",xterms,":")
+   print(x1)
+else:
+    print("Fibonacci sequence upto",xterms,":")
+    while compute < xterms:
+        print(x1)
+        xth = x1 + x2
+        x1 = x2
+        x2 = xth
+        compute += 1
+
+
+#No.14
+
+def new_list():
+
+    s = [1,2,2,3,4,4,5,6,6,7,8,8,9,9,10]
+    s = list(dict.fromkeys(s))
+    print(s)
+
+new_list()
+
+#No.15
+
+z = 'Hello everyone' 
+reversed =''.join(reversed(z)) 
+print(reversed) 
+    
+
+#No.16
+import random
+import string
+def password(stringLength=10):
+    letters = string.ascii_lowercase + string.punctuation + string.digits
+    return ''.join(random.choice(letters) for i in range(stringLength))
+print (password() )
+print (password(10))
+
+#No.17
+# import requests
+# from bs4 import BeautifulSoup
+ 
+# base_url = 'http://www.nytimes.com'
+# r = requests.get(base_url)
+# soup = BeautifulSoup(r.text)
+ 
+# for story_heading in soup.find_all(class_="story-heading"): 
+#     if story_heading.a: 
+#         print(story_heading.a.text.replace("\n", " ").strip())
+#     else: 
+#         print(story_heading.contents[0].strip())
+
+
+#No.18
+import random
+
+def compare_numbers(number, user_guess):
+    cowbull = [0,0] 
+    for i in range(len(number)):
+        if number[i] == user_guess[i]:
+            cowbull[1]+=1
+        else:
+            cowbull[0]+=1
+    return cowbull
+
+if __name__=="__main__":
+    playing = True 
+    number = str(random.randint(0,9999)) 
+    guesses = 0
+
+    print("The game is called Cowbull!") 
+    print("I will say a number, and you have to guess the numbers.")
+    print("if the number is wrong, you get a cow. If it's right, you get a bull.")
+    print("The game ends when you get 4 bulls!")
+    print("Type exit to end the game.")
+
+#No.19
+# import requests
+# from bs4 import BeautifulSoup
+
+# base_url = "http://www.vanityfair.com/society/2014/06/monica-lewinsky-humiliation-culture"
+# r = requests.get(base_url)
+# soup = BeautifulSoup(r.text)
+
+# all_p_cn_text_body = soup.select("div.parbase.cn_text > div.body > p")
+
+# for elem in all_p_cn_text_body[7:]:
+#   print(elem.text)
+
+
+#No.20
+def list(ordered_list, numbers):
+  for items in ordered_list:
+    if items == numbers:
+      print('Yes, the number is in the list a')
+  print ('No, the number is not in the list a') 
+  
+if __name__=="__main__":
+  a = [1,2,3,4,5,6,7]
+  print(list(a, 5)) 
+  print(list(a, 10)) 
+  
